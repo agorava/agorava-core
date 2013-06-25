796e260704ed0bd0cb06242f510a8f3bf92f8780
Antoine Sabot-Durand <antoine@sabot-durand.net>
2013-06-25 10:38:20 +0200

    Upgrade to Deltaspike 0.4 Correcting Arquillian test packaging to support Beans in Deltaspike jar

--- agorava-core-impl-cdi/src/test/java/org/agorava/core/cdi/test/AgoravaExtensionOverrideServiceTest.java
+++ agorava-core-impl-cdi/src/test/java/org/agorava/core/cdi/test/AgoravaExtensionOverrideServiceTest.java
@@ -20,62 +20,26 @@ import junit.framework.Assert;
 import org.agorava.core.api.oauth.OAuthService;
 import org.agorava.core.api.oauth.OAuthSession;
 import org.agorava.core.cdi.Current;
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ArchivePath;
-import org.jboss.shrinkwrap.api.Filter;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
-import java.io.FileNotFoundException;
 
 /**
  * @author Antoine Sabot-Durand
  */
 
 @RunWith(Arquillian.class)
-public class AgoravaExtensionOverrideServiceTest {
+public class AgoravaExtensionOverrideServiceTest extends AgoravaTestDeploy {
 
     @Inject
     @FakeService
     OAuthService service;
-
     @Inject
     @FakeService
     @Current
     OAuthSession session;
-
-
-    @Deployment
-    public static Archive<?> createTestArchive() throws FileNotFoundException {
-        JavaArchive testJar = ShrinkWrap.create(JavaArchive.class, "all-agorava.jar")
-
-                .addPackages(true, new Filter<ArchivePath>() {
-                    @Override
-                    public boolean include(ArchivePath path) {
-                        return !(path.get().contains("test"));
-                    }
-                }, "org.agorava")
-                .addAsResource("META-INF/services/javax.enterprise.inject.spi.Extension")
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-
-
-        WebArchive ret = ShrinkWrap
-                .create(WebArchive.class, "test.war")
-                .addClasses(AgoravaExtensionTestProducers.class, FakeRoot.class, FakeService.class, FakeService2.class, FakeServiceLiteral.class, FakeService2Literal.class, FakeServiceImpl.class)
-                .addAsLibraries(testJar)
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-
-        System.out.println(System.getProperty("arquillian"));
-        return ret;
-    }
-
 
     @Test
     public void testIsServiceOverrided() throws Exception {
